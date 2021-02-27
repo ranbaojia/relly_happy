d7924842ec882225c34b052fb31fc7f2b38dc678
ranbaojia <463533099@qq.com>
2021-02-27 10:58:22 +0800

    主支第八次提交

--- js/index_load.js
+++ js/index_load.js
@@ -4,3 +4,5 @@ var $foot = $('#foot');
 $foot.load('./sub_html/foot.html');
 var $column = $('#column');
 $column.load("./sub_html/fix.html");
+var $fixSearch = $('#fixSearch');
+$fixSearch.load('./sub_html/fix_sub.html');
